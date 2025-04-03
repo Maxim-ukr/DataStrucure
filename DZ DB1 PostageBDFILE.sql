edec56491fbe7ee0849070a60398141fa2b35003
Maxim-ukr <Kovalenkomn1984@gmail.com>
2025-04-03 18:53:06 +0300

    ДЗ postgres1

---
+++ DZ DB1 PostageBDFILE.sql
@@ -0,0 +1,23 @@
+--КОМЕНТАРІ
+-- СТВОРИТИ БАЗУ ДАНИХ
+CREATE DATABASE CENTER
+
+-- ЗМІНИТИ БД(ПЕРЕІМЕНУВАТИ)
+ALTER DATABASE CENTERP RENAME TO CENTER
+
+-- ВИДАЛИТИ БАЗУ ДАНИХ
+DROP DATABASE IF EXISTS CENTER
+
+-- HOMEWORK
+-- Завдання 1
+-- Створіть базу даних під назвою Birds. Розташування залишається на ваш вибір.
+CREATE DATABASE Birds
+
+-- Завдання 2
+-- Переназвіть базу даних із першого завдання. Нове ім’я
+-- для бази даних Cats.
+ALTER DATABASE Birds RENAME TO Cats
+
+-- Завдання 3
+-- Видаліть базу даних Cats.
+DROP DATABASE IF EXISTS Cats
